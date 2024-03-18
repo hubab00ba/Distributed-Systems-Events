5317d396336ba156f1494fdae7b044147a99fe61
Mark Dubrovskyi <m.dubrovskyi@gmail.com>
2024-03-18 13:40:31 +0100

    Initial commit

---
+++ first-task/src/main/java/com/intellias/distributed/firsttask/FirstTaskApplication.java
@@ -0,0 +1,13 @@
+package com.intellias.distributed.firsttask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstTaskApplication.class, args);
+	}
+
+}
